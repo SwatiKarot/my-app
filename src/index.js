3bcfb2b943e701ead4f2d2bbd95b165c168bf0d4
Swati Karot <v-skarot@microsoft.com>
2021-10-29 14:55:03 +0530

    changed the token

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.render(
   c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
   t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
   y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-})(window, document, "clarity", "script", "92n73tpryi");
+})(window, document, "clarity", "script", "92ycrplao9");
 
 
 // If you want to start measuring performance in your app, pass a function
