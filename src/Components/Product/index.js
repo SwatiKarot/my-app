5f33894099e5cc1ecc914733a04e5d13af5267f4
Swati Karot <v-skarot@microsoft.com>
2021-11-02 15:33:45 +0530

    added reducers and hoc

--- src/Components/Product/index.js
+++ src/Components/Product/index.js
@@ -1,22 +1,80 @@
-import React, { Component } from 'react';
-import { Card, ListGroup } from 'react-bootstrap';
+import React from 'react';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
 
-class Products extends Component {
-    render() {
-        return (
-            <div style={{ display: 'flex', justifyContent: 'center', padding: 30 }}>
-                <div><h2>Products Page</h2>
-                    <Card style={{ width: '18rem' }}>
-                        <ListGroup>
-                            <ListGroup.Item>Product 1</ListGroup.Item>
-                            <ListGroup.Item>Product 2</ListGroup.Item>
-                            <ListGroup.Item>Product 3</ListGroup.Item>
-                        </ListGroup>
-                    </Card>
-                </div>
-            </div>
-        );
-    }
+// tracking
+import { withTracking } from 'react-tracker';
+import { purchaseEvent } from '../../tracking/events/productEvents';
+
+import styles from './product.css';
+
+const Product = ({
+  id,
+  name,
+  price,
+  starred,
+  purchase,
+  starProduct,
+  purchaseProduct,
+  trackPurchaseProduct,  
+}) => {
+
+    return (
+      <li className={styles.Product}>
+        <div className={styles.productInfos}>
+          <div>
+            <span className="Product__name">
+              {name}
+            </span>
+          </div>
+          <div>
+            <span className="Friend__price">
+              {price} $
+            </span>
+          </div>
+        </div>
+        <div className={styles.productActions}>
+          <button title="Star this product" className={`btn btn-default ${styles.btnAction}`}
+                  onClick={() => starProduct(id)}>
+            <i className={classnames('fa', {
+              'fa-star': starred,
+              'fa-star-o': !starred
+            })} />
+          </button>
+          <button title="Purchase this product" className={`btn btn-default ${styles.btnAction}`}
+                  onClick={() => {
+                    // In additional to dispatch redux action to add product to cart with given id
+                    // And since we configured our middleware will also
+                    // fire the event listener with eventType 'PURCHASE_PRODUCT'
+                    // Redux action object will be passed to the event listener as first argument
+                    purchaseProduct(id);
+
+                    // Imagine that purchase product is not a redux action
+                    // so you need to track this manually!
+                    trackPurchaseProduct(id);
+
+                    // So this event will be tracked twice !!
+                    } }>
+            <i className="fa fa-plus" />
+          </button>
+        </div>
+      </li>
+    );
 }
 
-export default Products;
+Product.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  starred: PropTypes.bool,
+  starProduct: PropTypes.func.isRequired
+};
+
+const mapTrackingToProps = trackEvent => ({
+  trackPurchaseProduct: (id, name, price) =>
+    trackEvent(purchaseEvent(id, name, price))
+});
+
+const ProductWithTracking = withTracking(mapTrackingToProps)(Product)
+
+export default ProductWithTracking;
